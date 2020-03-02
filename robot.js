76c4e3834286bbd02542e9132dcd27556ce0f212
danysai <48719605+danysainero@users.noreply.github.com>
2020-03-02 10:21:23 +0100

    add move to tank

--- robot.js
+++ robot.js
@@ -2,12 +2,65 @@
 
 async function main(tank) {
 
+  // variables
+  const ARENA_WIDTH = 1340;
+  const ARENA_HEIGHT = 1000;
+  const BREAK_DISTANCE = 100;
+
+  const SAFETY_AREA = {
+    left: BREAK_DISTANCE,
+    rigth: (ARENA_WIDTH - BREAK_DISTANCE),
+    bottom: BREAK_DISTANCE,
+    top: (ARENA_HEIGHT - BREAK_DISTANCE),
+  };
+
   // auxiliary functions
 
+  async function moveController(tank) {
 
-  // main loop
+    let x = await tank.getX();
+    let y = await tank.getY();
 
-  while (true) {
+    if (x >= (ARENA_WIDTH / 2)) {
+      await tank.drive(0, 49);
+    } else {
+      await tank.drive(180, 49);
+    }
+
+    if (await tank.getX() > SAFETY_AREA.rigth) {
+      await tank.drive(90, 49);
+      while (await tank.getY() < SAFETY_AREA.top) {
+        await tank.drive(90, 49);
+      }
+    }
+
+     if (await tank.getY() > SAFETY_AREA.top) {
+      await tank.drive(180, 49);
+      while (await tank.getX() > SAFETY_AREA.left) {
+        await tank.drive(180, 49);
+      }
+    }
+
+     if (await tank.getX() < SAFETY_AREA.left) {
+      await tank.drive(270, 49);
+      while (await tank.getY() > SAFETY_AREA.bottom) {
+        await tank.drive(270, 49);
+      }
+    }
+
+    if (await tank.getX() < SAFETY_AREA.bottom) {
+      console.log('bottom');
+      
+      await tank.drive(0, 49);
+      while (await tank.getX() < SAFETY_AREA.rigth) {
+        await tank.drive(0, 49);
+      }
+    }
+  }
+
   
+  // main loop
+  while (true) {
+    await moveController(tank);
   }
 }
