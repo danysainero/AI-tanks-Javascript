2cbf49c597e8ece5f57bb0f3f866f9e70f315505
danysai <48719605+danysainero@users.noreply.github.com>
2020-03-02 16:43:46 +0100

    tank scan in 4 directions

--- robot.js
+++ robot.js
@@ -5,60 +5,80 @@ async function main(tank) {
   // variables
   const ARENA_WIDTH = 1340;
   const ARENA_HEIGHT = 1000;
-  const BREAK_DISTANCE = 100;
+  const BREAK_DISTANCE = 160;
+  const MAX_SCAN_ANGLE = 10;
+  const MAX_SAFETY_SPEED = 69;
+  const BREAK_SPEED = 0;
 
   const SAFETY_AREA = {
-    left: BREAK_DISTANCE,
-    rigth: (ARENA_WIDTH - BREAK_DISTANCE),
+    left: (BREAK_DISTANCE + 50),
+    rigth: (ARENA_WIDTH - (BREAK_DISTANCE + 50)),
     bottom: BREAK_DISTANCE,
     top: (ARENA_HEIGHT - BREAK_DISTANCE),
   };
+
+  const ANGLE_DIRECTION = {
+    left: 180,
+    right: 0,
+    top: 90,
+    bottom: 270
+  }
 
   // auxiliary functions
 
   async function moveController(tank) {
 
+    //Start drive
     let x = await tank.getX();
-    let y = await tank.getY();
-
     if (x >= (ARENA_WIDTH / 2)) {
-      await tank.drive(0, 49);
+      await tank.drive(ANGLE_DIRECTION.right, MAX_SAFETY_SPEED);
     } else {
-      await tank.drive(180, 49);
+      await tank.drive(ANGLE_DIRECTION.left, MAX_SAFETY_SPEED);
     }
 
+    //Drive top
     if (await tank.getX() > SAFETY_AREA.rigth) {
-      await tank.drive(90, 49);
+      console.log('drive top');
+      await tank.drive(ANGLE_DIRECTION.right, BREAK_SPEED);
       while (await tank.getY() < SAFETY_AREA.top) {
-        await tank.drive(90, 49);
+        await tank.drive(ANGLE_DIRECTION.top, MAX_SAFETY_SPEED);
+        await scanController(ANGLE_DIRECTION.left, tank);
       }
     }
 
-     if (await tank.getY() > SAFETY_AREA.top) {
-      await tank.drive(180, 49);
+    //Drive left
+    if (await tank.getY() > SAFETY_AREA.top) {
+      console.log('drive left');
+      await tank.drive(ANGLE_DIRECTION.left, BREAK_SPEED);
       while (await tank.getX() > SAFETY_AREA.left) {
-        await tank.drive(180, 49);
+        await tank.drive(ANGLE_DIRECTION.left, MAX_SAFETY_SPEED);
+        await scanController(ANGLE_DIRECTION.bottom, tank);
       }
     }
 
-     if (await tank.getX() < SAFETY_AREA.left) {
-      await tank.drive(270, 49);
+    //Drive down
+    if (await tank.getX() < SAFETY_AREA.left) {
+      await tank.drive(ANGLE_DIRECTION.bottom, BREAK_SPEED);
       while (await tank.getY() > SAFETY_AREA.bottom) {
-        await tank.drive(270, 49);
+        await tank.drive(ANGLE_DIRECTION.bottom, MAX_SAFETY_SPEED);
+        await scanController(ANGLE_DIRECTION.right, tank);
       }
     }
 
+    //Drive right
     if (await tank.getX() < SAFETY_AREA.bottom) {
-      console.log('bottom');
-      
-      await tank.drive(0, 49);
+      await tank.drive(ANGLE_DIRECTION.right, BREAK_SPEED);
       while (await tank.getX() < SAFETY_AREA.rigth) {
-        await tank.drive(0, 49);
+        await tank.drive(ANGLE_DIRECTION.right, MAX_SAFETY_SPEED);
+        await scanController(ANGLE_DIRECTION.top, tank);
       }
     }
   }
 
-  
+  async function scanController(angle, tank) {
+    let scanResult = await tank.scan((angle), MAX_SCAN_ANGLE);
+  }
+
   // main loop
   while (true) {
     await moveController(tank);
